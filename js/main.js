d76689d4892a0f0053b68388e02a51c830d5e600
Piorek1994 <pprus94@gmail.com>
2023-01-06 16:24:32 +0100

    media queries

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 import custom from './custom'
-window.addEventListener("load", () => {
-    custom()
-});
-
-
-
+window.addEventListener('load', () => {
+	custom()
+})
 const playBtn = document.querySelector('.hero_section-play')
 
-console.log(playBtn)
+function addVideo() {
+	const div = document.createElement('div')
+	div.innerHTML =
+		'<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+        div.classList.add("showVideo")
+	document.body.appendChild(div)
+}
+
+
+
+
+playBtn.addEventListener('click', addVideo)
